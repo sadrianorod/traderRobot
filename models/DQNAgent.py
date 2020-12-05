031e3c228cfe0369b05d8cebf6acdb7787da0a46
lucasbilobran <skybilobran@gmail.com>
2020-12-05 05:27:14 -0300

    add trade method to dqn

--- models/DQNAgent.py
+++ models/DQNAgent.py
@@ -11,7 +11,7 @@ from gym.utils import seeding
 from DQN.agent import QAgent
 from utils import get_scaler, get_data
 
-from setup import START_MONEY
+from setup import START_MONEY, LISTED_COMPANIES_NAMES
 import time
 
 class DQNAgent(BackTestInterface):
@@ -20,38 +20,9 @@ class DQNAgent(BackTestInterface):
         #self.clf = None
         
 
-    def setup(self,dbars):
-        """  assets=['VALE3']
-        if len(assets)!=1:
-            print('Error, this trader is supposed to deal with just one asset')
-            return None
-        bars=dbars[assets[0]]
-        # remove irrelevant info
-        if 'time' in bars:
-            del bars['time']
-        timeFrame=10 # it takes into account the last 10 bars
-        horizon=1 # it project the closing price for next bar
-        target='close' # name of the target column
-        ds=self.b3.ai_utils.bars2Dataset(bars,target,timeFrame,horizon)
-
-        X=self.b3.ai_utils.fromDs2NpArrayAllBut(ds,['target'])
-        discretizer = KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='uniform') 
-
-        # creates the discrete target
-        ds['target']=self.b3.ai_utils.discTarget(discretizer,ds['target'])
-
-        
-        Y=self.b3.ai_utils.fromDs2NpArray(ds,['target'])
-        clf = tree.DecisionTreeClassifier()
-        clf = clf.fit(X, Y)
-        self.clf=clf """
-
-        ## Entao, aqui eu preciso de uma função que popule a variavel train_data baseado em dbars
-        ## tipo, agora eu fiz uma que pega as informações dos csv salvos, mas a gente vai querer
-        ## uma que use aquela starDate e Predate EU ACHO.
-        train_data = np.around(get_data(dbars))
-        
+    def setup(self, dbars):
         # data
+        train_data = np.around(get_data(dbars))
         self.stock_price_history = np.around(train_data) # round up to integer to reduce state space
         self.n_stock, self.n_step = self.stock_price_history.shape
         print(self.n_stock,self.n_step)
@@ -64,7 +35,8 @@ class DQNAgent(BackTestInterface):
         self.cash_in_hand = None
 
         # action space
-        self.action_space = spaces.Discrete(5**self.n_stock)
+        self.action_space = spaces.Discrete(3**self.n_stock)
+        self.action_combo = [*map(list, itertools.product([0, 1, 2], repeat=self.n_stock))]
 
         # observation space: give estimates in order to sample and build scaler
         stock_max_price = self.stock_price_history.max(axis=1)
@@ -72,6 +44,7 @@ class DQNAgent(BackTestInterface):
         price_range = [[0, mx] for mx in stock_max_price]
         cash_in_hand_range = [[0, self.init_invest * 2]]
         self.observation_space = spaces.MultiDiscrete(stock_range + price_range + cash_in_hand_range)
+
         # seed and start
         self.seed()
         self.reset()
@@ -81,72 +54,67 @@ class DQNAgent(BackTestInterface):
         self.agent = QAgent(state_size, action_size)
         self.scaler = get_scaler(self.stock_price_history, self.init_invest, self.n_stock)
 
-        # training parameters
-        self.episode = 2000
-        self.batch_size = 32
-        timestamp = time.strftime('%Y%m%d%H%M')
+        # parameters
+        self.batch_size = 500
 
         # here we could have a variable called 'train'. If it is true we train, otherwise we load from weight file. 
-        portfolio_value = []
-        for e in range(self.episode): # change this number to size of train data
-            state = self.reset()
-            state = self.scaler.transform([state])
-            for time in range(self.n_step):
-                action = self.agent.act(state)
-                next_state, reward, done, info = self.step(action)
-                next_state = self.scaler.transform([next_state])
-                self.agent.remember(state, action, reward, next_state, done)
-                state = next_state
-                if done:
-                    print("episode: {}/{}, episode end value: {}".format(
-                    e + 1, self.episode, info['cur_val']))
-                    portfolio_value.append(info['cur_val']) # append episode end portfolio value
-                    break
-                if len(self.agent.memory) > self.batch_size:
-                    self.agent.replay(self.batch_size)
-            if (e + 1) % 10 == 0:  # checkpoint weights
-                self.agent.save('weights/{}-dqn.h5'.format(timestamp))
+        
+        # here we train =]
+        state = self.reset()
+        state = self.scaler.transform([state])
+        for time in range(self.n_step):
+            action = self.agent.act(state)
+            next_state, reward, done = self.train_step(action)
+            next_state = self.scaler.transform([next_state])
+            self.agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                break
+            if len(self.agent.memory) > self.batch_size: # train faster with this 
+                self.agent.replay(self.batch_size)
+            
+        
+        self.last_state = self.reset()
+        self.last_state = self.scaler.transform([self.last_state])
 
-        # save portfolio value history to disk
-        with open('portfolio_val/{}-{}.p'.format(timestamp, "train"), 'wb') as fp:
-            pickle.dump(portfolio_value, fp)
-
-    def ending(self,dbars):
+    def ending(self, dbars):
         pass
 
-    def trade(self,bts,dbars):
-            assets=['VALE3'] #só utiliza 1 ação nessa implementação
-            orders=[]
-            timeFrame=10 # it takes into account the last 10 bars
-            horizon=1 # it project the closing price for next bar
-            target='close' # name of the target column
-            for asset in assets:
-                curr_shares=self.get_current_shares(asset)
-                money=self.get_current_money()/len(assets) # divide o saldo em dinheiro igualmente entre os ativos
-                free_shares=self.get_affordable_shares(dbars,asset)
-                # get new information (bars), transform it in X
-                bars=dbars[asset]
-                #remove irrelevant info
-                if 'time' in bars:
-                    del bars['time']
-                # convert from bars to dataset
-                ds=self.b3.ai_utils.bars2Dataset(bars,target,timeFrame,horizon)
-                # Get X fields
-                X=self.b3.ai_utils.fromDs2NpArrayAllBut(ds,['target'])
+    def trade(self, bts, dbars):
+        self.state = self.update_state(bts, dbars)
+        self.state = self.scaler.transform([self.state])
+        action = self.agent.act(self.state)
+        action_vec = self.action_combo[action]
 
-                # predict the result, using the latest info
-                p=self.clf.predict([X[-1]])
-                if p==2:
-                    #buy it
-                    order=self.buy_order(asset,free_shares)
-                elif p==0:
-                    #sell it
-                    order=self.sell_order(asset,curr_shares)
-                else:
-                    order=None
-                if order!=None:
-                    orders.append(order)
-            return orders   
+
+        sell_assets = []
+        buy_assets = []
+        assets = LISTED_COMPANIES_NAMES
+        for i, a in enumerate(action_vec):
+            if a == 0:
+                sell_assets.append(assets[i])
+            elif a == 2:
+                buy_assets.append(assets[i])
+
+        orders=[]
+        for asset in sell_assets:
+            curr_shares=self.get_current_shares(asset)
+            order=self.sell_order(asset,curr_shares)
+            orders.append(order)
+        
+        money=self.get_current_money()/len(buy_assets)
+        for asset in buy_assets:
+            order=self.buy_order(asset,money)
+            orders.append(order)
+
+        reward = self.get_reward()
+        self.agent.remember(self.last_state, action, reward, self.state, False)
+        self.last_state = self.state
+
+        if len(self.agent.memory) > self.batch_size:
+                self.agent.replay(self.batch_size)
+
+        return orders   
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -161,12 +129,12 @@ class DQNAgent(BackTestInterface):
         return self.get_obs()
 
 
-    def step(self, action):
+    def train_step(self, action):
         assert self.action_space.contains(action)
-        reward, cur_val = self.get_reward(action)
+        self.cur_step += 1
+        reward = self.train_get_reward(action)
         done = self.cur_step == self.n_step - 1
-        info = {'cur_val': cur_val}
-        return self.get_obs(), reward, done, info
+        return self.get_obs(), reward, done
 
 
     def get_obs(self):
@@ -180,12 +148,55 @@ class DQNAgent(BackTestInterface):
     def get_val(self):
         return np.sum(self.stock_owned * self.stock_price) + self.cash_in_hand 
     
-    def get_reward(self, action):
+
+    def train_get_reward(self, action):
         prev_val = self.get_val()
-        self.cur_step += 1
         self.stock_price = self.stock_price_history[:, self.cur_step] # update price
-        self._trade(action)
+        self.train_trade(action)
         cur_val = self.get_val()
         reward = cur_val - prev_val
 
-        return reward, cur_val
+        return reward
+    
+    
+    def get_reward(self):
+        ##        np.sum(self.stock_owned * self.stock_price) + self.cash_in_hand 
+        prev_val = np.sum(self.last_state[:5] * self.last_state[5:10]) + self.cash_in_hand 
+
+        cur_val = self.get_val()
+        reward = cur_val - prev_val
+
+        return reward
+
+    def train_trade(self, action):
+        # just update update self.cash_in_hand and self.stock_owned
+        action_vec = self.action_combo[action]
+        sell_assets = []
+        buy_assets = []
+        for i, a in enumerate(action_vec):
+            if a == 0:
+                sell_assets.append(i)
+            elif a == 2:
+                buy_assets.append(i)
+        
+        money=self.cash_in_hand()/len(buy_assets)
+        for asset in buy_assets:
+            ## TODO: tem que conferir como que o get_affor_shares funciona
+            if self.stock_price[asset] < money:
+                n_shares = int ( money/self.stock_price[asset] )
+                self.stock_owned[asset] += n_shares
+                self.cash_in_hand -= self.stock_price[asset] * n_shares
+        
+        for asset in sell_assets:
+            # sell it all
+            self.cash_in_hand += self.stock_price[asset] * self.stock_owned[asset]
+            self.stock_owned[asset] = 0
+
+
+    def update_state(self, bts, dbars):
+        ## Aqui tenho que atualizar baseado no bts e dbars o:
+        # self.stock own, que eh o vetor de assets que eu tenho [asset1, asset2, ..., asset5]
+        # self.stock_price, que eh o vetor do preço atual das assets agora [price1, ..., price5]
+        self.cash_in_hand = self.get_current_money()
+
+        return self.get_obs()
